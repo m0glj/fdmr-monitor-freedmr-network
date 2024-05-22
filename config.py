8d521abd7a50ee2e504009a935e6b3fa22dcfc49
M0GLJ <32222213+m0glj@users.noreply.github.com>
2024-05-22 18:00:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -24,11 +24,12 @@ from pathlib import Path
 
 
 __author__ = 'Christian Quiroz, OA4DOA'
+__modified__ = 'Adrian Craig, M0GLJ'
 __verion__ = '1.0.0'
 __copyright__ = 'Copyright (c) 2022 Christian Quiroz, OA4DOA'
 __copyright__ = 'Copyright (c) 2024 Adrian Craig, M0GLJ'
 __license__ = 'GNU GPLv3'
-__maintainer & modified__ = 'Adrian Craig, M0GLJ'
+__maintainer__ = 'Adrian Craig, M0GLJ'
 __email__ = 'admin@oz-dmr.network'
 
 logger = logging.getLogger("fdmr-mon")
